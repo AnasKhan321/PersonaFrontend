fa0d9441faeb97943425303b1d033d51fa43949d
Anas <anascoder4@gmail.com>
2025-01-13 19:07:32 +0530

    changes

--- app/clientComponents/chat.jsx
+++ app/clientComponents/chat.jsx
@@ -15,16 +15,16 @@ export default function Chat(data) {
     e.preventDefault();
     
     if (message.trim()) {
-      setChatHistory((prev) => [...prev, { sender: 'You', content: message }]);
-  
-      socket.emit("message", JSON.stringify({ username: data.username, question: message }));
+      setChatHistory((prev) => [...prev, { sender: 'user', content: message }]);
+      console.log(chatHistory)
+      socket.emit("message", JSON.stringify({ username: data.username, question: message   , messages : chatHistory}));
       
       setMessage('');
   
       socket.once("send:message", (data) => {
         const pdata = JSON.parse(data);
         console.log(pdata);
-        setChatHistory((prev) => [...prev, { sender: pdata.username, content: pdata.message }]);
+        setChatHistory((prev) => [...prev, { sender: "assistant", content: pdata.message }]);
       });
     }
   };
@@ -36,10 +36,10 @@ export default function Chat(data) {
 
         <div className="bg-gray-800 rounded-lg p-4 h-[75vh] max-h-[75vh]  overflow-y-scroll mb-4">
           {chatHistory.map((msg, index) => (
-            <div key={index} className={`mb-4 ${msg.sender === 'You' ? 'text-right' : ''}`}>
+            <div key={index} className={`mb-4 ${msg.sender === 'user' ? 'text-right' : ''}`}>
 
-              {msg.sender === "You" ? 
-              <span className={`inline-block p-2 rounded-lg ${msg.sender === 'You' ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-100'}`}>
+              {msg.sender === "user" ? 
+              <span className={`inline-block p-2 rounded-lg ${msg.sender === 'user' ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-100'}`}>
                 {msg.content}
               </span>  : <div className='bg-gray-700 text-gray-100 p-2 rounded-md '>  <ReactTyped strings={[msg.content]} typeSpeed={10}   loop={false}/> </div>  }
             </div>
